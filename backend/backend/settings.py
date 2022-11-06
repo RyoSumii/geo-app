b4ddcd8a314f9a3b52e5197c06807760b766f642
rsumii <ryobasketball0408@gmail.com>
2022-11-06 15:28:02 +0900

    adding api end point

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,9 +44,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'rest_framework',
+    'rest_framework_gis',
 
     'listings',
     'users',
+
 ]
 
 MIDDLEWARE = [
